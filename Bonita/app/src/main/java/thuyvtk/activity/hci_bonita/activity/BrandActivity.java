83748d8139b06d65acd81cf72c4e1e6352caefa9
Vo Thi Kim Thuy <vokimthuy211@gmail.com>
2019-12-16 11:47:53 +0700

    [thuyVTK]

--- Bonita/app/src/main/java/thuyvtk/activity/hci_bonita/activity/BrandActivity.java
+++ Bonita/app/src/main/java/thuyvtk/activity/hci_bonita/activity/BrandActivity.java
@@ -2,14 +2,27 @@ package thuyvtk.activity.hci_bonita.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 import thuyvtk.activity.hci_bonita.R;
 
 public class BrandActivity extends Activity {
+    ImageButton btnBack;
+    ImageButton btnSave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_brand);
+        defineView();
+    }
+
+    public void defineView() {
+        btnBack = findViewById(R.id.btnBack);
+        btnSave = findViewById(R.id.btnSave);
+        btnBack.getBackground().setAlpha(150);
+        btnSave.getBackground().setAlpha(150);
+
+
     }
 }
