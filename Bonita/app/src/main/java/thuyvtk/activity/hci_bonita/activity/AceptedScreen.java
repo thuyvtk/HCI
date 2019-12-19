6f053b7060237b8c3ca4eab687be50475c510b5d
Vo Thi Kim Thuy <vokimthuy211@gmail.com>
2019-12-19 12:13:38 +0700

    [Accept screen]

--- Bonita/app/src/main/java/thuyvtk/activity/hci_bonita/activity/AceptedScreen.java
+++ Bonita/app/src/main/java/thuyvtk/activity/hci_bonita/activity/AceptedScreen.java
@@ -3,8 +3,10 @@ package thuyvtk.activity.hci_bonita.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import thuyvtk.activity.hci_bonita.R;
 
@@ -18,5 +20,17 @@ public class AceptedScreen extends Activity {
 
     public void clickToBack(View view) {
         this.finish();
+    }
+
+    public void CancelOrder(View view) {
+        Toast.makeText(this, "Bạn đã hủy đặt chỗ !", Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(this, MainPageActivity.class);
+        startActivity(intent);
+    }
+
+    public void useCode(View view) {
+        Toast.makeText(this, "Bạn đã sử dụng mã đặt chỗ thành công!", Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(this, MainPageActivity.class);
+        startActivity(intent);
     }
 }
